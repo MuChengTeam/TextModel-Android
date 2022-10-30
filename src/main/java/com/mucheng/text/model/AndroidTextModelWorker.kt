4c7a46547992c0fc75cd43fd04baa8eb15ca6c96
CaiMuCheng <2632588830@qq.com>
2022-10-30 15:41:13 +0800

    Fixed AndroidTextModelWorker#outputToWriter save unknown empty char

--- src/main/java/com/mucheng/text/model/AndroidTextModelWorker.kt
+++ src/main/java/com/mucheng/text/model/AndroidTextModelWorker.kt
@@ -62,7 +62,7 @@ object AndroidTextModelWorker {
                 }
                 val textRow = iterator.next()
                 val unsafeValue = textRow.unsafeValue()
-                writer.write(unsafeValue, 0, unsafeValue.size)
+                writer.write(unsafeValue, 0, textRow.length)
                 if (iterator.hasNext()) {
                     writer.write(newLine)
                 }
